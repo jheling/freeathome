739687788651cbabd0c373b9c7ffce27f71c1f6b
Jeroen van de Erve <jeroenvde@gmail.com>
2022-08-14 20:11:45 +0200

    black style formatting

--- custom_components/freeathome/fah/crypto.py
+++ custom_components/freeathome/fah/crypto.py
@@ -10,8 +10,7 @@ from .constants import General, Result, FAHMessage
 log = logging.getLogger(__name__)
 
 
-class Crypto():
-
+class Crypto:
     def __init__(self, jid, password, iterations, salt):
         self.jid = jid
         self.password = password
@@ -29,9 +28,11 @@ class Crypto():
         self.publicKey = keypair[0]
         self.secretKey = keypair[1]
 
-    # complete 
+    # complete
     def generateSharedKey(self):
-        return hashlib.pbkdf2_hmac('sha256', bytes(self.password, 'utf-8'), self.salt, self.iterations)
+        return hashlib.pbkdf2_hmac(
+            "sha256", bytes(self.password, "utf-8"), self.salt, self.iterations
+        )
 
         # complete
 
@@ -49,12 +50,12 @@ class Crypto():
         generic_hash = libnacl.crypto_generichash(message, key)
 
         if generic_hash is None:
-            raise Error('generic hash undefined')
+            raise Error("generic hash undefined")
 
         token = libnacl.crypto_onetimeauth(self.publicKey, generic_hash)
 
         if len(self.publicKey) + len(key) + len(token) != 64:
-            raise Error('Unexpected token size')
+            raise Error("Unexpected token size")
 
         authenticator = bytearray(self.publicKey)
         authenticator.extend(key)
@@ -65,30 +66,35 @@ class Crypto():
     def completeKeyExchange(self, data):
 
         if len(data) < 8:
-            raise Error('Invalid KeyExchange response')
+            raise Error("Invalid KeyExchange response")
 
         pos = 0
 
-        keyExchangeVersion = int.from_bytes(data[pos:pos + 4], "little")
+        keyExchangeVersion = int.from_bytes(data[pos : pos + 4], "little")
 
         pos += 4
         log.info(keyExchangeVersion)
 
         if keyExchangeVersion != 2:
-            raise Exception('Unexpected KeyExchange version %', keyExchangeVersion)
+            raise Exception("Unexpected KeyExchange version %", keyExchangeVersion)
 
-        errorCode = int.from_bytes(data[pos: pos + 4], "little")
+        errorCode = int.from_bytes(data[pos : pos + 4], "little")
         pos += 4
 
         if errorCode != 0 and errorCode != 25:
-            raise Exception('received error code % as result of KeyExchange ', errorCode)
+            raise Exception(
+                "received error code % as result of KeyExchange ", errorCode
+            )
 
         if len(data) < (pos + 16 + 16):
-            raise Exception('Insufficient data length in KeyExchange response, have only % bytes ', len(data))
+            raise Exception(
+                "Insufficient data length in KeyExchange response, have only % bytes ",
+                len(data),
+            )
 
-        fD = data[pos: pos + 16]
+        fD = data[pos : pos + 16]
         pos += 16
-        fS = data[pos: pos + 16]
+        fS = data[pos : pos + 16]
         pos += 16
 
         sharedKey = self.generateSharedKey()
@@ -96,47 +102,57 @@ class Crypto():
         authenticatorValid = self.validateAuthenticator(data[pos:], fD, fS, sharedKey)
 
         if authenticatorValid is None:
-            raise Exception('Failed to authenticate key exchange data')
+            raise Exception("Failed to authenticate key exchange data")
 
         fK = self.extractData(data, pos)
-        pos += fK['length']
-        sessionIdentifier = fK['data']
+        pos += fK["length"]
+        sessionIdentifier = fK["data"]
 
         flags = self.extractData(data, pos)
-        pos += flags['length']
+        pos += flags["length"]
 
         if len(data) < (pos + 32):
             raise Exception(
-                'KeyExchange response buffer too short, expected 32 bytes for public key at pos' + pos + ",have length " + len(
-                    data))
+                "KeyExchange response buffer too short, expected 32 bytes for public key at pos"
+                + pos
+                + ",have length "
+                + len(data)
+            )
 
-        publicKey = data[pos:pos + 32]
+        publicKey = data[pos : pos + 32]
 
-        self.cryptoIntermediateData = libnacl.crypto_box_beforenm(publicKey, self.secretKey)
+        self.cryptoIntermediateData = libnacl.crypto_box_beforenm(
+            publicKey, self.secretKey
+        )
 
         return sessionIdentifier
 
     def extractData(self, fX, fY):
         if len(fX) < (fY + 4):
-            raise Exception('Cannot read string from buffer, buffer not large enough')
+            raise Exception("Cannot read string from buffer, buffer not large enough")
 
         fW = fX
-        length = int.from_bytes(fX[fY:(fY + 3)], "little", )
+        length = int.from_bytes(
+            fX[fY : (fY + 3)],
+            "little",
+        )
 
         if length == 0:
-            raise Exception('Failed to read keyId from KeyExchange response')
+            raise Exception("Failed to read keyId from KeyExchange response")
 
         if length > 20000000:
-            raise Exception('Cannot read string from buffer, string length exceeds allowed maximum size')
+            raise Exception(
+                "Cannot read string from buffer, string length exceeds allowed maximum size"
+            )
 
         if length > (len(fX) - fY - 4):
-            raise Exception('Cannot read string from buffer at pos %', fY)
+            raise Exception("Cannot read string from buffer at pos %", fY)
 
-        result = fX[(fY + 4):(fY + 4 + length)]
+        result = fX[(fY + 4) : (fY + 4 + length)]
 
-        result_ascii = result.decode('utf-8')
+        result_ascii = result.decode("utf-8")
 
-        SystemAccessPointResponse = {'data': result_ascii, 'length': 4 + length}
+        SystemAccessPointResponse = {"data": result_ascii, "length": 4 + length}
 
         return SystemAccessPointResponse
 
@@ -156,24 +172,31 @@ class Crypto():
         data_bytes = base64.b64decode(data)
 
         if data_bytes is None or len(data_bytes) == 0:
-            raise Exception('Can not decrypt empty pubsub')
+            raise Exception("Can not decrypt empty pubsub")
 
-        nonce = data_bytes[0:libnacl.crypto_box_NONCEBYTES]
+        nonce = data_bytes[0 : libnacl.crypto_box_NONCEBYTES]
 
         messageReader = MessageReader(data_bytes[16:24])
         nonceNumber = messageReader.readUint64()
-        if 'update' not in self.__Yq:
-            self.__Yq['update'] = {}
-            self.__Yq['update']['sequenceCounter'] = 'cU'  # Was this supposed to be cU (without quotes)?
-            self.__Yq['update']['skippedSymmetricSequences'] = []
+        if "update" not in self.__Yq:
+            self.__Yq["update"] = {}
+            self.__Yq["update"][
+                "sequenceCounter"
+            ] = "cU"  # Was this supposed to be cU (without quotes)?
+            self.__Yq["update"]["skippedSymmetricSequences"] = []
 
-        sequenceNumber = self.__Yq['update']['sequenceCounter']
+        sequenceNumber = self.__Yq["update"]["sequenceCounter"]
         if nonceNumber < sequenceNumber:
-            if nonceNumber not in self.__Yq['update']['skippedSymmetricSequences']:
-                raise Exception('Unexpected sequence in received symmetric nonce ',
-                                nonceNumber, '(', sequenceNumber, ')')
+            if nonceNumber not in self.__Yq["update"]["skippedSymmetricSequences"]:
+                raise Exception(
+                    "Unexpected sequence in received symmetric nonce ",
+                    nonceNumber,
+                    "(",
+                    sequenceNumber,
+                    ")",
+                )
 
-            self.__Yq['update']['skippedSymmetricSequences'].remove(nonceNumber)
+            self.__Yq["update"]["skippedSymmetricSequences"].remove(nonceNumber)
 
         if nonceNumber > sequenceNumber:
             c = nonceNumber - sequenceNumber - 1
@@ -185,21 +208,24 @@ class Crypto():
                 if x == 0:
                     break
 
-            self.__Yq['update']['skippedSymmetricSequences'].append(x)
+            self.__Yq["update"]["skippedSymmetricSequences"].append(x)
             x -= x
 
-            if len(self.__Yq['update']['skippedSymmetricSequences']) > 32:
-                a = sorted(self.__Yq['update']['skippedSymmetricSequences'])  # returns list
+            if len(self.__Yq["update"]["skippedSymmetricSequences"]) > 32:
+                a = sorted(
+                    self.__Yq["update"]["skippedSymmetricSequences"]
+                )  # returns list
                 dK = len(a) - 32
                 for i in range(dK):
-                    self.__Yq['update']['skippedSymmetricSequences'].remove(a[i])
+                    self.__Yq["update"]["skippedSymmetricSequences"].remove(a[i])
 
-        self.__Yq['update']['sequenceCounter'] += 1
+        self.__Yq["update"]["sequenceCounter"] += 1
 
-        pubSubMessage = libnacl.crypto_secretbox_open_easy(data_bytes[libnacl.crypto_box_NONCEBYTES:], nonce,
-                                                           self.__Key)
+        pubSubMessage = libnacl.crypto_secretbox_open_easy(
+            data_bytes[libnacl.crypto_box_NONCEBYTES :], nonce, self.__Key
+        )
         if pubSubMessage is None:
-            raise Exception('Failed to decrypt message')
+            raise Exception("Failed to decrypt message")
 
         return pubSubMessage
 
@@ -211,11 +237,11 @@ class Crypto():
 
         result = data.readUint32()
         if result != Result.RESULT_CODE_OK:
-            raise Exception('Failed to establish session')
+            raise Exception("Failed to establish session")
 
         fah_version = data.readUint32()
         if fah_version != General.PROTOCOL_VERSION:
-            log.info('Unknown Protocol Version detected. Ignoring ...', fah_version)
+            log.info("Unknown Protocol Version detected. Ignoring ...", fah_version)
 
         self.__Ys = data.readString()
         self.__Yt = data.readBlob(8)
@@ -228,14 +254,14 @@ class Crypto():
         cl.writeUint32(0)
 
         if self.messageCounter > 4294967296:
-            raise Exception('MessageCounter exceeds valid range')
+            raise Exception("MessageCounter exceeds valid range")
 
         cl.writeBlob(libnacl.randombytes_buf(8))
         return cl.toUint8Array()
 
     def encryptPayload(self, data):
         if len(data) > 10485760:
-            raise Exception('encryptPayload: message is too large: ', len(data))
+            raise Exception("encryptPayload: message is too large: ", len(data))
 
         cq = 0
         if not self.__Yv:
@@ -244,7 +270,7 @@ class Crypto():
         nonce = self.createNonce()
 
         if nonce is None:
-            raise Exception('MessageCounter exceeds valid range')
+            raise Exception("MessageCounter exceeds valid range")
 
         cn = len(nonce) + General.FH_CRYPTO_MAC_LENGTH + len(data)
         ct = bytearray(libnacl.randombytes_buf(libnacl.crypto_box_NONCEBYTES))
@@ -253,12 +279,14 @@ class Crypto():
         cm += ct
         cm += data
 
-        cr = libnacl.crypto_box_easy_afternm(bytes(cm), bytes(nonce), self.cryptoIntermediateData)
+        cr = libnacl.crypto_box_easy_afternm(
+            bytes(cm), bytes(nonce), self.cryptoIntermediateData
+        )
         if cr is None or len(cr) == 0:
-            raise Exception('Failed to encrypt message')
+            raise Exception("Failed to encrypt message")
 
         if len(cr) != cn:
-            raise Exception('Internal error: Unexpected size of encrypted data array')
+            raise Exception("Internal error: Unexpected size of encrypted data array")
 
         self.__Yp.append(ct)  # append random nonce bytes to list
 
@@ -272,7 +300,7 @@ class Crypto():
 
         return co.toUint8Array()
 
-    def decryptPayload(self, message):  # data is of type messagereader        
+    def decryptPayload(self, message):  # data is of type messagereader
 
         data = MessageReader(message)
 
@@ -283,16 +311,24 @@ class Crypto():
         messageData = data.getRemainingData()
 
         if len(messageData) != messageLength:
-            raise Exception('Failed to decrypt container: invalid message length ', messageLength, ' expected ',
-                            len(messageData))
+            raise Exception(
+                "Failed to decrypt container: invalid message length ",
+                messageLength,
+                " expected ",
+                len(messageData),
+            )
 
         if messageLength < libnacl.crypto_box_MACBYTES:
-            raise Exception('Failed to decrypt container: invalid message length ', messageLength)
+            raise Exception(
+                "Failed to decrypt container: invalid message length ", messageLength
+            )
 
         cX = None
 
         for x in self.__Yp:
-            cX = libnacl.crypto_box_open_easy_afternm(bytes(messageData), bytes(x), self.cryptoIntermediateData)
+            cX = libnacl.crypto_box_open_easy_afternm(
+                bytes(messageData), bytes(x), self.cryptoIntermediateData
+            )
             if cX is not None:
                 self.__Yp.remove(x)
                 break
@@ -305,18 +341,18 @@ class Crypto():
 
             for i in range(numNames):
                 cO = keyData.readString()
-                cL = cO.rfind('/')
+                cL = cO.rfind("/")
                 if cL == -1:
                     continue
 
-                cR = cO[cL + 1:]
-                if cR.endswith('_encrypted'):
-                    cR = cR[0:len(cR) - 10]
+                cR = cO[cL + 1 :]
+                if cR.endswith("_encrypted"):
+                    cR = cR[0 : len(cR) - 10]
 
                 cU = keyData.readUint64()
                 self.__Yq[cR] = {}
-                self.__Yq[cR]['sequenceCounter'] = cU
-                self.__Yq[cR]['skippedSymmetricSequences'] = []
+                self.__Yq[cR]["sequenceCounter"] = cU
+                self.__Yq[cR]["skippedSymmetricSequences"] = []
 
             self.__Yv = True
 
@@ -330,7 +366,9 @@ class Crypto():
         clientFinal = self.clientScramHandler.createClientFinal()
 
         if clientFinal is None or len(clientFinal) == 0:
-            raise Exception('Failed to send login response message, failed to create clientFinal')
+            raise Exception(
+                "Failed to send login response message, failed to create clientFinal"
+            )
 
         return clientFinal
 
@@ -346,7 +384,7 @@ class Crypto():
 def loginSaslPayload(scram):
     messageWriter = MessageWriter()
     messageWriter.writeUint8(FAHMessage.MSG_ID_LOGIN_SASL)
-    messageWriter.writeString('SCRAM-SHA-256')
+    messageWriter.writeString("SCRAM-SHA-256")
     messageWriter.writeString(scram)
     return messageWriter.toUint8Array()
 
