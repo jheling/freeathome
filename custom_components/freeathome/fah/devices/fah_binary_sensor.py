14a1f1903eb6a3c1272b02bd10c7cd986ab0085a
jheling <helingj@gmail.com>
2024-01-29 20:02:24 +0100

    smoke detector + carbon monoxide dectecter

--- custom_components/freeathome/fah/devices/fah_binary_sensor.py
+++ custom_components/freeathome/fah/devices/fah_binary_sensor.py
@@ -19,6 +19,8 @@ from ..const import (
         PID_SWITCHOVER_HEATING_COOLING,
         PID_MOVEMENT_UNDER_CONSIDERATION_OF_BRIGHTNESS,
         PID_PRESENCE,
+        PID_FIRE_ALARM_ACTIVE,
+        PID_CO_ALARM_ACTIVE,
         )
 
 LOG = logging.getLogger(__name__)
@@ -50,6 +52,8 @@ class FahBinarySensor(FahDevice):
                         # Keep movement detector here, although in tests it only ever showed value '1'
                         PID_MOVEMENT_UNDER_CONSIDERATION_OF_BRIGHTNESS,
                         PID_PRESENCE,
+                        PID_FIRE_ALARM_ACTIVE,
+                        PID_CO_ALARM_ACTIVE,
                         ]
                     }
 
@@ -64,6 +68,14 @@ class FahBinarySensor(FahDevice):
 
         self.state = '0' if value == '0' else '1'
         LOG.info("binary sensor %s (%s) dp %s state %s", self.name, self.lookup_key, dp, value)
-            
+
+    def is_fire_sensor(self):
+        """Return true if device is a dimmer"""
+        return PID_FIRE_ALARM_ACTIVE in self._datapoints
+
+    def is_co_sensor(self):
+        """Return true if device is a dimmer"""
+        return PID_CO_ALARM_ACTIVE in self._datapoints
+
     def update_parameter(self, param, value):
         LOG.debug("Not yet implemented")
