4f681567134d302f3782924068be9b3c928f91ab
Matthias Pröll <13684520+matze19999@users.noreply.github.com>
2026-08-13 10:58:01 +0200

    Fix deprecated concentration unit constants

--- custom_components/freeathome/sensor.py
+++ custom_components/freeathome/sensor.py
@@ -3,12 +3,12 @@ import logging
 
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import (
+    PERCENTAGE,
+    UnitOfDensity,
+    UnitOfRatio,
     UnitOfSpeed,
     UnitOfTemperature,
-    PERCENTAGE,
-    CONCENTRATION_PARTS_PER_MILLION,
-    CONCENTRATION_MICROGRAMS_PER_CUBIC_METER
-    )
+)
 
 from homeassistant.components.sensor import SensorEntity
 
@@ -50,12 +50,12 @@ SENSOR_TYPES = {
         SensorDeviceClass.HUMIDITY],
     "co2": [
         "CO2",
-        CONCENTRATION_PARTS_PER_MILLION,
+        UnitOfRatio.PARTS_PER_MILLION,
         None,
         SensorDeviceClass.CO2],
     "voc": [
         "VOC",
-        CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+        UnitOfDensity.MICROGRAMS_PER_CUBIC_METER,
         None,
         SensorDeviceClass.VOLATILE_ORGANIC_COMPOUNDS],
 }
